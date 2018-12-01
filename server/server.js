d810517c5e9a9a8e0aeb7250cf5c87309826785a
Dan MacKay <docblues@DanMacKbook.local>
2018-12-01 17:42:53 -0600

    trying different build

--- server/server.js
+++ server/server.js
@@ -7,8 +7,16 @@ const nextStopRouter = require('./routes/nextStop.router');
 //check that server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// Serve static files
-app.use(express.static('build'));
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
 
 // Body parser middleware
 app.use(bodyParser.json());
